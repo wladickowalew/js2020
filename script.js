1dc9da0376f3c73efce75ac2f1b4f77d83fdef76
Wlad Kowalew <wladickowalew@gmail.com>
2020-01-26 14:03:23 +0300

    punkt 2_1

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("all ok")
+punkt2_1()
+
+function punkt2_1() {
+	//разница в сравнении
+	let d = {key: "value"};
+	let e = {key: "value"} == d;  //нестрогое сравнение
+	console.log(e);
+	let a = {key: "value"} === d; //строгое сравнение
+	console.log(a);
+	let b = d;
+	let c = b === d;              //ссылаются на один и тот же объект
+	console.log(c);
+}
